6b3b6134ba6801e3d8746e9a1195e1f262d97964
ndortonne-coder <ndortonne@gmail.com>
2026-02-26 16:47:02 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+namespace screenMagic {
+/**
+* Address LEDs linearly row first
+*/
+//% blockId=screenmagicplotat
+//% block="plot at index"
+//% index.min=0 index.max=25
+export function plotAt(index: number): void {
+index = 0
+const y = Math. floor (index / 5);
+const x = Math. floor(index % 5);
+led.plot(x, y)
+}
+  }
