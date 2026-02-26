5560359a86b997a39ffccd2605d008549d23de44
ndortonne-coder <ndortonne@gmail.com>
2026-02-26 16:51:14 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,9 +6,9 @@ namespace screenMagic {
 //% block="plot at index"
 //% index.min=0 index.max=25
 export function plotAt(index: number): void {
-index = 0
+index |= 0
 const y = Math. floor (index / 5);
 const x = Math. floor(index % 5);
 led.plot(x, y)
 }
-  }
+}
